d922aa7983b719d7afbfa8e923fc3779ed955280
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-06-15 09:21:11 +0300

    Updates and cleanups

--- microservices/ui/src/server.js
+++ microservices/ui/src/server.js
@@ -1,5 +1,6 @@
-var path = require("path");
-var express = require("express");
+/* eslint-disable */
+const path = require("path");
+const express = require("express");
 
 var app = express();
 
@@ -7,5 +8,5 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.set("port", process.env.PORT || 8080);
 
 var server = app.listen(app.get("port"), function () {
-  console.log("Listening on port ", server.address().port);
+  console.log("listening on port ", server.address().port);
 });
