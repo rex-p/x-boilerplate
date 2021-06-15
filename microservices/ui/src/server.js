a2609d5a956c796a0a69a7bbdcd050808912e4c9
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-06-15 09:31:38 +0300

    (deployment)

--- microservices/ui/src/server.js
+++ microservices/ui/src/server.js
@@ -5,8 +5,11 @@ const express = require("express");
 var app = express();
 
 app.use(express.static(path.join(process.cwd(), "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "dist", "index.html"));
+});
 app.set("port", process.env.PORT || 8080);
 
 var server = app.listen(app.get("port"), function () {
-  console.log("listening on port ", server.address().port);
+  console.log("Listening on port: ", server.address().port);
 });
